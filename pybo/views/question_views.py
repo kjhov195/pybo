cffb7165cda347cd1ab4f4f02dceddc42b49ccf3
kjhov195 <kjhov195@gmail.com>
2023-05-12 17:21:03 +0900

    Add pages

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from pybo.models import Question
 
@@ -7,7 +7,9 @@ bp = Blueprint('question', __name__, url_prefix='/question')
 
 @bp.route('/list/')
 def _list():
+    page = request.args.get('page', type=int, default=1)  # 페이지
     question_list = Question.query.order_by(Question.create_date.desc())
+    question_list = question_list.paginate(page=page, per_page=10)
     return render_template('question/question_list.html', question_list=question_list)
 
 
